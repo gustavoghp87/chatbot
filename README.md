04126f8d0dd8464d00821ca72b3f4d90afe5090a
gustavoghp87 <ghp.2120@gmail.com>
2020-12-03 12:15:09 -0300

    first

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chatbot
 Chatbot in Python with ChatterBot and ChatterBot_Corpus libraries
+
+Use Python 3.7 with Anaconda (TensorFlow)
---
+++ main.py
@@ -0,0 +1,61 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import pandas as pd
+
+
+bot = ChatBot(
+    'MaslaChat',  
+    logic_adapters = ['chatterbot.logic.BestMatch']
+)
+trainer = ListTrainer(bot)
+
+data = pd.read_excel('frases.xlsx')
+
+list = []
+
+for i in data.index:
+    try:
+        clave = str(data['clave'][i])
+        frase = str(data['frase'][i])
+        print(i)
+        if clave!='nan':
+            list.append(clave)
+            print(clave)
+        list.append(frase)
+        print(frase)
+    except:
+        print("error")
+
+    if i > 60:
+        break
+
+
+trainer.train(list)
+
+#trainer = ChatterBotCorpusTrainer(bot)
+# trainer.train("chatterbot.corpus.spanish")
+# trainer.train("chatterbot.corpus.spanish.greetings")
+# trainer.train("chatterbot.corpus.spanish.conversations")
+# trainer.train("chatterbot.corpus.spanish.dinero")
+# trainer.train("chatterbot.corpus.spanish.IA")
+# trainer.train("chatterbot.corpus.spanish.emociones")
+# trainer.train("chatterbot.corpus.spanish.perfilbot")
+# trainer.train("chatterbot.corpus.spanish.psicologia")
+# trainer.train("chatterbot.corpus.spanish.trivia")
+
+
+print("\n")
+
+name = input("Ingresa tu nombre: ")
+
+print('\n')
+
+while True:
+    request = input(name.upper() + ':  ')
+    if request=='Bye' or request =='bye':
+        print('MASLACHAT: Bye')
+        break
+    else:
+        response=bot.get_response(request)
+        print('MASLACHAT: ', response, '\n')
